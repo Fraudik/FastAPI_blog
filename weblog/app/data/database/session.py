319d920a14dfe44b027b8d3b4352b2c8a6763bf4
Fraudik <52631426+Fraudik@users.noreply.github.com>
2022-09-03 20:01:37 +0300

    Version 1.0

--- weblog/app/data/database/session.py
+++ weblog/app/data/database/session.py
@@ -7,8 +7,6 @@ from sqlalchemy.orm import sessionmaker
 
 from data.configuration import settings
 
-#SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@db:5432/app"
-
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
